1d9530b381d30945b9370289c49262a5d63c42fc
Christopher Boyd <boyd.christopher.t@gmail.com>
2023-10-03 17:29:52 -0500

    broke the text out into a component

---
+++ src/components/App/App.jsx
@@ -0,0 +1,10 @@
+import Intro from '../Intro/Intro';
+import './App.css';
+
+function App() {
+  return (
+    <Intro />
+  );
+}
+
+export default App;
---
+++ src/components/Intro/Intro.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default funcion Intro() {
+    return (
+        <div>
+            <h1>Who is Lee Raymond and why is he worse than Adolf Hitler?</h1>
+            <p>
+                This isn't just Godwin's Law hyperbole. Lee Raymond is the person who holds the single most responsibility for the climate crisis that anyone in the world could have. He is close to the prime cause of climate denial.
+            </p>
+            <p>
+                How could anyone hold the most responsibility for the climate crisis, you ask? Well... let me show you.
+            </p>
+        </div>
+    );
+}
